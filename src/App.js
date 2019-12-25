269e3ba2b4d470f67284089985a446fbcfdbf9a9
Amy Latimer <56357845+amylatimer@users.noreply.github.com>
2019-12-25 09:32:24 -0500

    added component layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Hero from './components/Hero/Hero';
 import Info from './components/Info/Info';
+import TicksSuck from './components/Projects/TicksSuck/TicksSuck';
 
 function App() {
   return (
     <>
+      <div className="App">
+        <Info />
+        <div className="project-container">
+          <TicksSuck />
+          <TicksSuck />
+        </div>
+
+      </div>
       {/* <div className="App">
         <header className="App-header">
           <h1>Coming Soon!</h1>
@@ -13,7 +22,7 @@ function App() {
 
       </div> */}
       {/* <Hero /> */}
-      <Info />
+
 
     </>
   );
--- src/components/Projects/TicksSuck/TicksSuck.js
+++ src/components/Projects/TicksSuck/TicksSuck.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './TicksSuck.scss';
 
+import desk from '../../../assets/images/ticksSuck/ts-desk.svg';
+
 class TicksSuck extends React.Component {
     state = {}
     render() {
         return (
             <section className="project">
                 <div className="project__container">
+                    <div className="project__image-container">
+                        <img className="project__image--desk" src={desk} alt="" />
+                        {/* <img className="project__image--tablet" src={} alt="" /> */}
+                    </div>
+                    <div className="project__text-container">
+                        <h1 className="project__title">Ticks Suck</h1>
+                        <h2 className="project__type">Website</h2>
+                        <p className="project__info">Short Description of the project</p>
+                        <p className="project__tech">React, SCSS, Node, Express, Axios, React Router, Dark Sky API</p>
 
+                    </div>
                 </div>
 
             </section>
